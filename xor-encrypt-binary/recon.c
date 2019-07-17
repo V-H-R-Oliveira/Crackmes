aa6570d42b559395d556350a6a8e7ee308928789
Vitor Oliveira <30324984+V-H-R-Oliveira@users.noreply.github.com>
2019-07-17 20:45:25 -0300

    Update recon.c

--- xor-encrypt-binary/recon.c
+++ xor-encrypt-binary/recon.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
-#include <sys/syscall.h>
 
 int main(int argc, char **argv) {
     if(argc == 2) 
     {
-        printf("%ld\n", *(int64_t *)argv[1]);
         if (*(int64_t *)argv[1] == 0x212164656e7770) {
             puts("parabens, voce conseguiu ^.^");
             return 0;
